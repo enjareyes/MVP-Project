d6e7e6f5cc331703f9520a5d1b0231bbb2e1a9b2
Enja Reyes <enja.reyes@gmail.com>
2015-06-09 20:33:00 -0500

    working again

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ var express = require('express');
     mongo = require('mongodb'),
     MongoClient = require('mongodb').MongoClient, 
     assert = require('assert');
-     
 
+     
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() { console.log('Node app running on port', app.get('port')) });
 app.use(express.static('./client'));
@@ -20,6 +20,34 @@ MongoClient.connect(url, function(err, db) {
   db.close();
 });
 
+// exports = module.exports = app;
 
-exports = module.exports = app;
+app.get('/searchfood',function(req,res){
+  var foodName = req.query.food,
+      url = 'http://api.nal.usda.gov/usda/ndb/search/?format=json&q=' + foodName + '&sort=n&max=25&offset=0&api_key=kKJ078H1u9KjuD4DLAJK3nPUgFX4SoN2awG94IeR'
+  
+  request( url, function(err, response, body){
+    if (err) console.log('Error in /searchFood');
+    else {
+      res.set('Content-Type', 'application/json')
+      res.status(200).send(body)
+    }
+  })
+})
+
+app.get('/foodinfo',function(req,res){
+  var ndbno = req.query.ndbno,
+      url = 'http://api.nal.usda.gov/usda/ndb/reports/?ndbno=' + ndbno +'&type=b&format=json&api_key=kKJ078H1u9KjuD4DLAJK3nPUgFX4SoN2awG94IeR'
+  
+  request( url, function(err, response, body){
+    if (err) console.log('Error in /foodinfo');
+    else {
+      res.set('Content-Type', 'application/json')
+      res.status(200).send(body)
+    }
+  })
+})
+
+
+
 
